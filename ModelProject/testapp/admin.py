1a53b1a0f8c059985095ca1cdc5749f395f10d29
aman2191 <79457243+aman2191@users.noreply.github.com>
2021-12-29 11:46:28 +0530

    add admin file

--- ModelProject/testapp/admin.py
+++ ModelProject/testapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from django.contrib.admin.sites import AdminSite
+from testapp.models import *
 # Register your models here.
+
+class PublisherAdmin(admin.ModelAdmin):
+    list_display=['name','address','city','country','wbsite']
+admin.site.register(Publisher,PublisherAdmin)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display=['first_name','last_name','emails']
+admin.site.register(Author,AuthorAdmin)
+
+class BookAdmin(admin.ModelAdmin):
+    list_dispaly=['title','author','publisher']
+admin.site.register(Book,BookAdmin)
