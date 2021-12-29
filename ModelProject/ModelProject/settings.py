6191fc89a9286d9f41d4093b215ceaa9d66faea8
aman2191 <79457243+aman2191@users.noreply.github.com>
2021-12-29 11:44:46 +0530

    third modification

--- ModelProject/ModelProject/settings.py
+++ ModelProject/ModelProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'testapp',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'ModelProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'BOOKSHOP',
+        'USER':'root',
+        'PASSWORD':'aman2191'
     }
 }
 
--- ModelProject/testapp/models.py
+++ ModelProject/testapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+    address=models.CharField(max_length=100)
+    city=models.CharField(max_length=100)
+    country=models.CharField(max_length=50)
+    wbsite=models.URLField()
+    
+    def __str__(self):
+        return self.name
+    
+class Author(models.Model):
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    emails=models.EmailField()
+    
+    def __str__(self):
+        return self.first_name
+    
+class Book(models.Model):
+    title=models.CharField(max_length=100)
+    author=models.ManyToManyField(Author)
+    publisher=models.ForeignKey(Publisher,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
